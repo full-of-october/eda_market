3936026c8ab22e4e4577ab2a6d8135d359428699
DESKTOP-UK66V8F\User <full.of.october@gmail.com>
2023-02-06 16:20:03 +0200

    first commit

---
+++ js/main_js.js
@@ -0,0 +1,169 @@
+$('.recomends').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+const salesButton = document.getElementById('pills-profile-tab')
+salesButton.addEventListener('click', function () {
+  console.log('work');
+  setTimeout(function (params) {
+    $('.sales').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 990,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 560,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  }, 200)
+})
+
+
+
+$('.news').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+let buttonMenu = document.getElementById("menu-button")
+let menu = document.getElementById("collapseExample")
+
+buttonMenu.addEventListener("click", function () {
+
+  let hasClassShow = menu.classList.contains("show");
+
+  if (hasClassShow) {
+    menu.classList.remove("show");
+  } else {
+    menu.classList.add("show");
+  }
+
+  
+})
+
+
+let menuitem = document.getElementById("menuitemid")
+
+let submenu = document.getElementById("submenuid")
+
+menuitem.addEventListener("click", function () {
+
+  let hasClassShowSubmenu = submenu.classList.contains("show");
+
+  if (hasClassShowSubmenu) {
+    submenu.classList.remove("show");
+  } else {
+    submenu.classList.add("show");
+  }
+
+  
+})
+
+
+let openTriangle = document.getElementById("arrow")
+
+menuitem.addEventListener("click", function () {
+
+  let hasClassOpen = openTriangle.classList.contains("open-triangle");
+
+
+  if (hasClassOpen) {
+    openTriangle.classList.remove("open-triangle");
+  } else {
+    openTriangle.classList.add("open-triangle");
+  }
+
+  
+})
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+      extend: {
+          colors: {
+              'dark-blue': '#0E2149',
+          }
+      }
+  }
+}
